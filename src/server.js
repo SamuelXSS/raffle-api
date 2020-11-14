1486d583cc6882a84dae71b17cd6939449ebeccc
samuelxses@unipam.edu.br <Samuel Ximenes>
2020-11-13 22:43:37 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -9,4 +9,7 @@ require('./database')
 app.listen(process.env.PORT || 3000)
 app.use(express.json())
 app.use(routes)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin": "*")
+}) 
 app.use(cors())
